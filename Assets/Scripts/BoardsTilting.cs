9da0583488e92c21c18deb31d6333220df44df91
Dmitry Chirin <chirin.dima@gmail.com>
2025-04-10 01:56:02 +0200

    add access to debug overlay from another script

--- Assets/Scripts/BoardsTilting.cs
+++ Assets/Scripts/BoardsTilting.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class board: MonoBehaviour
 {
     public float smooth = 5.0f;
     public float rotateAngle = 30.0f;
+    [SerializeField] DebugOverlay debug;
 
     // Start is called before the first frame update
     void Start()
     {
-        GameObject player1 = GameObject.FindGameObjectWithTag("Player1");
-        GameObject player2 = GameObject.FindGameObjectWithTag("Player2");
-
+//         GameObject player1 = GameObject.FindGameObjectWithTag("Player1");
+//         GameObject player2 = GameObject.FindGameObjectWithTag("Player2");
+        debug.debugText.text = "Tilting";
 //         if (player1 != null)
 //             rb1 = player1.GetComponent<Rigidbody>();
 //         if (player2 != null)
@@ -25,9 +27,10 @@ public class board: MonoBehaviour
         // gets input from user
         float xRotation = Input.GetAxis("Horizontal")*rotateAngle;
         float zRotation = Input.GetAxis("Vertical")*rotateAngle;
-        // convert the rotation angles into quarternions.
-        Quaternion bRotation = Quaternion.Euler(xRotation, 0.0f, zRotation);
-        //rotate
-        transform.rotation = Quaternion.Slerp(transform.rotation, bRotation, Time.deltaTime*smooth);
+
+//         // convert the rotation angles into quarternions.
+//         Quaternion bRotation = Quaternion.Euler(xRotation, 0.0f, zRotation);
+//         // rotate
+//         transform.rotation = Quaternion.Slerp(transform.rotation, bRotation, Time.deltaTime*smooth);
     }
 }
