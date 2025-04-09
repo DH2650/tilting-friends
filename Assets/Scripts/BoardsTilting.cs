fae01c38c3f9a7dd5815c20a67013e2db8c27a41
Dmitry Chirin <chirin.dima@gmail.com>
2025-04-10 01:33:53 +0200

    add debug text overlay

--- Assets/Scripts/BoardsTilting.cs
+++ Assets/Scripts/BoardsTilting.cs
@@ -10,6 +10,13 @@ public class board: MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        GameObject player1 = GameObject.FindGameObjectWithTag("Player1");
+        GameObject player2 = GameObject.FindGameObjectWithTag("Player2");
+
+//         if (player1 != null)
+//             rb1 = player1.GetComponent<Rigidbody>();
+//         if (player2 != null)
+//             rb2 = player2.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -19,7 +26,7 @@ public class board: MonoBehaviour
         float xRotation = Input.GetAxis("Horizontal")*rotateAngle;
         float zRotation = Input.GetAxis("Vertical")*rotateAngle;
         // convert the rotation angles into quarternions.
-        Quaternion bRotation = Quaternion. Euler (xRotation, 0.0f, zRotation);
+        Quaternion bRotation = Quaternion.Euler(xRotation, 0.0f, zRotation);
         //rotate
         transform.rotation = Quaternion.Slerp(transform.rotation, bRotation, Time.deltaTime*smooth);
     }
