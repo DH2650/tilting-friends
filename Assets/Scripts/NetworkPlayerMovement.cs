196ce4a681825558634f9f3bd0560a478487eada
Dmitry Chirin <chirin.dima@gmail.com>
2025-05-07 14:24:18 +0200

    fix movement

--- Assets/Scripts/NetworkPlayerMovement.cs
+++ Assets/Scripts/NetworkPlayerMovement.cs
@@ -34,20 +34,20 @@ public class NetworkPlayerMovement : MonoBehaviour
             {
                 case "up_pressed":
                     // Start moving up or set a flag
-                    transform.Translate(Vector3.up * speed * Time.deltaTime); // Simple example
+                    transform.Translate(Vector3.back * speed * Time.deltaTime); // Simple example
                     break;
                 case "down_pressed":
-                    transform.Translate(Vector3.down * speed * Time.deltaTime);
+                    transform.Translate(Vector3.forward * speed * Time.deltaTime);
                     break;
                 // ... handle other inputs (left, right, actionA_pressed, etc.)
                 case "left_pressed":
-                    transform.Translate(Vector3.left * speed * Time.deltaTime);
-                    break;
-                case "right_pressed":
                     transform.Translate(Vector3.right * speed * Time.deltaTime);
                     break;
+                case "right_pressed":
+                    transform.Translate(Vector3.left * speed * Time.deltaTime);
+                    break;
                 case "actionA_pressed":
-                    Debug.Log($"Player {_playerId} pressed Action A!");
+                    transform.Translate(Vector3.up * speed * Time.deltaTime); // Simple example
                     // Perform action
                     break;
             }
