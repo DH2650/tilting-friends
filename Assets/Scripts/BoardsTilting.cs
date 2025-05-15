0a9c5f247b4601f5d24fc9f79ed93f7d2334e71d
Helin Saeid <helinsae@gmail.com>
2025-05-15 14:25:12 +0200

    player respawn
    player respawns when they fall off the board, also added materials for different players (not assigned yet)

--- Assets/Scripts/BoardsTilting.cs
+++ Assets/Scripts/BoardsTilting.cs
@@ -8,7 +8,9 @@ public class Board: MonoBehaviour
     public float smooth = 5.0f;
     public float rotateAngle = 30.0f;
     public float platformRadius = 10;
-    public float tiltCutoffY = -8;
+    public float tiltCutoffY = -7;
+    public float secondCutoff = -300;
+    public Vector3 respawnPosition = new Vector3(0, 5.0f, 0);
 
     [SerializeField] DebugOverlay debug;
     [SerializeField] Rigidbody board;
@@ -61,9 +63,14 @@ public class Board: MonoBehaviour
         {
             NetworkPlayerMovement ps = player.GetComponent<NetworkPlayerMovement>();
             Rigidbody rb = ps.rb;
-            if (rb.transform.position.y > tiltCutoffY)
+            if (rb.transform.position.y > tiltCutoffY){
                 norm = rb.transform.position;
 //             Debug.Log($"Player transform: {rb.transform.position}");
+            }
+            if (rb.transform.position.y < secondCutoff){
+                Respawn(rb);
+            }
+                
         }
 
         // Normalize x according to platformRadius distance from the centre
@@ -96,5 +103,13 @@ public class Board: MonoBehaviour
         // Rotate
         transform.rotation = Quaternion.Slerp(transform.rotation, bRotation, Time.deltaTime*smooth);
 //         arrow.transform.rotation = ogArrowRotation * transform.rotation;
+    }
+
+    void Respawn(Rigidbody player){
+        player.linearVelocity = Vector3.zero;
+        player.rotation = Quaternion.identity;
+        player.angularVelocity = Vector3.zero;
+        player.transform.position = respawnPosition;
+        
     }
 }
